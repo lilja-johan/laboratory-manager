e6ef6f6b1724b32d79b3badf869d95b35428b117
lilja-johan <jplilja93@gmail.com>
2022-03-17 20:58:34 +0100

    add sample model, service and test-data

---
+++ src/app/shared/model/sample.ts
@@ -0,0 +1,9 @@
+export interface Sample {
+  id: number;
+  name: string;
+  location: string;
+  desiredAnalysis: string[];
+  registeredAnalysis: string[];
+  completedAnalysis:string[];
+  status:string[];
+}
